08971178cf62275f2f81ec076deeb29dff49c8b6
Hasini-Local <hasini-local@CISE-MAC-069847.local>
2024-02-05 13:52:57 -0500

    Add rapidcheck sample test case.

--- sdsl-lite/main.cpp
+++ sdsl-lite/main.cpp
@@ -8,6 +8,14 @@ using namespace sdsl;
 
 int main()
 {
+    rc::check("double reversal yields the original value",
+            [](const std::vector<int> &l0) {
+              auto l1 = l0;
+              std::reverse(begin(l1), end(l1));
+              std::reverse(begin(l1), end(l1));
+              RC_ASSERT(l0 == l1);
+            });
 
+  return 0;
 
 }
